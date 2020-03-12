5f4ef57027e15870fa7325bf3efe7075fc54fbbd
vstark1327 <58952905+vstark1327@users.noreply.github.com>
2020-03-12 11:15:31 +0530

    Just Started

---
+++ View/CompanyDetails.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+class Company():
+    def __init__(self,master):
+        self.master=master
+
+
+
+
+
+
+
+top=Tk()
+w,h= top.winfo_screenwidth(),top.winfo_screenheight()
+obj=Company(top,w,h)
+top.mainloop()
+
