279a988063ca8464bbb648085cba957823fe3bce
vstark1327 <58952905+vstark1327@users.noreply.github.com>
2020-03-14 12:10:45 +0530

    SemiComplete Code

---
+++ View/RegisterPage.py
@@ -0,0 +1,116 @@
+from tkinter import *
+
+
+class Register(Frame):
+    def __init__(self):
+        # logreg.destroy()
+        registerTk = Tk()
+        registerTk.title("tinder For Jobs")
+        w, h = registerTk.winfo_screenwidth(), registerTk.winfo_screenheight()
+        registerTk.geometry("%dx%d+0+0" % (w, h))
+        super().__init__()
+        self.widthW = w
+        self.heightH = h
+        self.configure(background='blue')
+        self.pack(fill='both', expand=True)
+        self.createWidgets()
+        registerTk.mainloop()
+
+    def createWidgets(self):
+        """CREATING FRAMES"""
+        textFrame = Frame(self, bg='black', width=self.widthW, height=self.heightH / 4)
+        textFrame.grid(row=0, column=0, columnspan=2)
+
+        loginFrame = Frame(self, bg='black', width=self.widthW / 2 + 1, height=self.heightH * 0.75)
+        loginFrame.grid(row=1, column=0)
+
+        registerFrame = Frame(self, bg='black', width=self.widthW / 2, height=self.heightH * 0.75)
+        registerFrame.grid(row=1, column=1)
+
+        # self.grid_columnconfigure(0, uniform='g1')
+        # self.grid_columnconfigure(1, uniform='g1')
+        """FRAME ONE FOR LOGO"""
+        welcomeLabel1 = Label(textFrame, text="tinder", bg='blue', fg='white', font=('Chalet New York', 50))
+        welcomeLabel2 = Label(textFrame, text="for Jobs", bg='black', fg='white', font=('Chalet New York', 20))
+        welcomeLabel1.place(x=20, y=30)
+        welcomeLabel2.place(x=192, y=65)
+
+        """FRAME TWO: Registration As Applicant"""
+        applicantloginButton = Button(loginFrame, text='Login', width=20, height=2, bg='#434343', fg='white',
+                                      activebackground='#666666')
+        applicantloginButton.place(x=266, y=293)
+
+        applicantLabel = Label(loginFrame, text='Applicants Fill Here: ', bg='black', fg='white',
+                               font=('Chalet New York', 30))
+        applicantLabel.place(x=175, y=0)
+
+        TextLabelName = Label(loginFrame, text='Applicant Name: ', bg='black', fg='white',
+                              font=('Chalet New York',))
+        TextLabelName.place(x=195, y=90)
+
+        TextEntryName = Entry(loginFrame, bg='#434343', fg='black', width=30)
+        TextEntryName.place(x=330, y=93)
+
+        MonthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+                     'November', 'December']
+        DayList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+                   28, 29, 30, 31]
+        YearList = [1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980,
+                    1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996,
+                    1997, 1998, 1999, 2000]
+
+        TextLabelDOB = Label(loginFrame, text='DOB: ', bg='black', fg='white',
+                             font=('Chalet New York',))
+        TextLabelDOB.place(x=195, y=140)
+
+        variable1 = IntVar()
+        variable1.set(DayList[0])  # default value
+
+        variable2 = StringVar()
+        variable2.set(MonthList[0])  # default value
+
+        variable3 = IntVar()
+        variable3.set(YearList[0])  # default value
+
+        DayOption = OptionMenu(loginFrame, variable1, *DayList)
+        DayOption.config(bg='#434343', fg='white')
+        DayOption.place(x=250, y=138)
+
+        MonthOption = OptionMenu(loginFrame, variable2, *MonthList)
+        MonthOption.config(bg='#434343', fg='white')
+        MonthOption.place(x=330, y=138)
+
+        YearOption = OptionMenu(loginFrame, variable3, *YearList)
+        YearOption.config(bg='#434343', fg='white')
+        YearOption.place(x=450, y=138)
+
+        TextLabelPrompt = Label(loginFrame, text='Restart Your Search By Logging In.', bg='black',
+                                fg='white',
+                                font=('Chalet New York', 10))
+        TextLabelPrompt.place(x=235, y=350)
+
+        """FRAME THREe: Registration As Company"""
+        self.TextLabelFreshUser = Label(registerFrame, text='Fresher Here?', bg='black', fg='white',
+                                        font=('Chalet New York', 30))
+        self.TextLabelFreshUser.place(x=215, y=100)
+
+        self.TextLabelFirstSteps = Label(registerFrame, text='Take Your First Step!', bg='black',
+                                         fg='white',
+                                         font=('Chalet New York', 20))
+        self.TextLabelFirstSteps.place(x=206, y=175)
+
+        self.TextLabelPrompt = Label(registerFrame, text='Register Your Details.', bg='black',
+                                     fg='white',
+                                     font=('Chalet New York', 10))
+        self.TextLabelPrompt.place(x=277, y=350)
+
+        self.register = Button(registerFrame, text='Register', width=20, height=2, bg='#434343', fg='white',
+                               activebackground='#666666')
+        self.register.place(x=266, y=293)
+
+
+def createLogin():
+    log = Register()
+
+
+createLogin()
