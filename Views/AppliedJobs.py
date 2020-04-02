053a45b4dab714c906ee64ee5f189c9735e4e052
Varun Khatri <varun.h.khatri@gmail.com>
2020-04-02 08:56:48 +0530

    Literally one line change

--- Views/AppliedJobs.py
+++ Views/AppliedJobs.py
@@ -5,9 +5,9 @@ from Controllers.StateController import *
 
 
 class AppliedJobs(Frame):
-    def __init__(self,jobviewTk):
-        #self.join = JoinController()
-        #self.applicant_email_id = get_account().email_id
+    def __init__(self, jobviewTk):
+        self.join = JoinController()
+        self.applicant_email_id = get_account().email_id
         jobviewTk.destroy()
         self.appliedjobTk = Tk()
         self.appliedjobTk.title("tinder For Jobs")
@@ -23,9 +23,6 @@ class AppliedJobs(Frame):
 
     def createWidgets(self):
         """CREATING FRAMES"""
-        # [{'company_name': 'varun company', 'response': 'Applied', 'job_requirements': 'some requiremets... who cares'}]
-        # ESA AAYEGA LIST KE ANDAR DICTIONARY---------------------------------------------------------------------------
-        # SAMJHA?!!!!!!!!!!!!
 
         textFrame = Frame(self, bg='black', width=self.widthW, height=self.heightH / 4)
         textFrame.grid(row=0, column=0, columnspan=2)
@@ -56,14 +53,7 @@ class AppliedJobs(Frame):
         scrollBar.place(x=985, y=2, height=400)
 
         frameList = []
-        # ESA AAYEEEGGGAAAA -------------------------------------------------------------------
-        my_list = [{'company_name': 'varun company', 'response': 'Applied','job_requirements': 'some requiremets... who cares'},{'company_name': 'vinay company', 'response': 'Accepted',
-                    'job_requirements': 'Ethical Hacker'},{'company_name': 'abbas company', 'response': 'Applied',
-                    'job_requirements': 'Software Developer'},{'company_name': 'Sri1.0 company', 'response': 'Applied',
-                    'job_requirements': 'Bhikhari'},{'company_name': 'Sri2.0 company', 'response': 'Applied',
-                    'job_requirements': 'Saras Brother'},{'company_name': 'Daksh company', 'response': 'Applied',
-                    'job_requirements': 'fast bowler'}]
-        #var_get= self.join.get_applied_jobs(self.applicant_email_id))
+        my_list = self.join.get_applied_jobs(self.applicant_email_id)
         for val in my_list:
             frameList.append(Frame(innerFrame, bg='black', bd=1, relief='sunken'))
             frameList[-1].pack()
@@ -71,18 +61,21 @@ class AppliedJobs(Frame):
             statusVar = f"Status: {val['response']}"
             jobdescVar = f"Job Description: {val['job_requirements']}"
 
-            Label(frameList[-1], text=nameVar, bg='black', fg='white', width=90, height=3,font = ('Chalet New York', 15)).grid(row=0, columnspan=2)
+            Label(frameList[-1], text=nameVar, bg='black', fg='white', width=90, height=3,
+                  font=('Chalet New York', 15)).grid(row=0, columnspan=2)
 
-            Label(frameList[-1], text=jobdescVar, bg='black', fg='white', width=90, height=3,font = ('Chalet New York', 15)).grid(row=1, columnspan=2)
+            Label(frameList[-1], text=jobdescVar, bg='black', fg='white', width=90, height=3,
+                  font=('Chalet New York', 15)).grid(row=1, columnspan=2)
 
-            Label(frameList[-1], text=statusVar, bg='black', fg='white', width=90, height=3,font = ('Chalet New York', 15)).grid(row=2, columnspan=2)
+            Label(frameList[-1], text=statusVar, bg='black', fg='white', width=90, height=3,
+                  font=('Chalet New York', 15)).grid(row=2, columnspan=2)
             viewCanvas.create_window(0, 0, anchor='nw', window=innerFrame)
-            #viewCanvas.create_window(0, 0, anchor='se', window=innerFrame)
+            # viewCanvas.create_window(0, 0, anchor='se', window=innerFrame)
             viewCanvas.update_idletasks()
             viewCanvas.configure(scrollregion=viewCanvas.bbox('all'), yscrollcommand=scrollBar.set)
 
     def back(self):
-            JobView.jobview(self.appliedjobTk)
+        JobView.jobview(self.appliedjobTk)
 
 
 def appliedjobs(jobviewTk):
